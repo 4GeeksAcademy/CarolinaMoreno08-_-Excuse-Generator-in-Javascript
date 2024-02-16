85158f4ae1e9da8bbb63e629ffc874ebaff95ac6
CarolinaMoreno08 <152432657+CarolinaMoreno08@users.noreply.github.com>
2024-02-16 03:41:10 +0000

    second commit

--- main.js
+++ main.js
@@ -3,22 +3,31 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
 
+var btn = document.getElementById("button1");
+
+
+
 function excuseFunction() {
   
-  let whoRandom = Math.floor(Math.random()*3);
-  let actionRandom = Math.floor(Math.random()*3);
-  let whatRandom = Math.floor(Math.random()*2);
-  let whenRandom = Math.floor(Math.random()*4);
+  let whoRandom = Math.floor(Math.random()*who.length);
+  let actionRandom = Math.floor(Math.random()*action.length);
+  let whatRandom = Math.floor(Math.random()*what.length);
+  let whenRandom = Math.floor(Math.random()*when.length); 
 
-  
 
   let excuseString = [who[whoRandom],action[actionRandom],what[whatRandom],when[whenRandom]].join(' ');
   document.getElementById("myExcuse").innerHTML = excuseString;
   
-
 }
 
+btn.addEventListener("click", function(){
+  excuseFunction();
+});
 
+
+  // window.onload=function() {
+  //   excuseFunction();
+  // };
 
 
 
